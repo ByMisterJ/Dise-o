463966784b2c738cb3deb8c886ea9427cc9ddeda
daw2 <josearanda9000@gmail.com>
2023-12-19 14:04:27 +0100

    commit añadiendo en JavaScript el ejercicio blur

---
+++ JavaScript/01/blur.js
@@ -0,0 +1,8 @@
+//Funcion para pasar a mayusculas los caracteres introducidos en los inputs con el id: pasmas//
+// 01.js
+
+function myFunction() {
+    let x = document.getElementById("pasmas");
+    x.value = x.value.toUpperCase();
+  }
+  
