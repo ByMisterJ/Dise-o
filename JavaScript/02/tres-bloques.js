21b391b341eb70eb2334463a83bac6b90a1c2015
daw2 <josearanda9000@gmail.com>
2023-12-21 13:27:59 +0100

    commit de la parte de tres bloques finalizada

--- JavaScript/02/tres-bloques.js
+++ JavaScript/02/tres-bloques.js
@@ -1,3 +1,39 @@
-function bloq_bloq(){
+function handleMouseOver(color1) {
+    var mensaje1
 
+    switch (color1) {
+        case 'rojo':
+            mensaje1 = 'Entraste al bloque rojo'
+            break
+        case 'blanco':
+            mensaje1 = 'Entraste al bloque blanco'
+            break
+        case 'azul':
+            mensaje1 = 'Entraste al bloque azul'
+            break
+        default:
+            mensaje1 = 'Entraste al lugar'
+    }
+
+    alert(mensaje1)
+}
+
+function handleMouseOut(color2) {
+    var mensaje2
+
+    switch (color2) {
+        case 'rojo':
+            mensaje2 = 'Saliste desde el bloque rojo'
+            break
+        case 'blanco':
+            mensaje2 = 'Saliste desde el bloque blanco'
+            break
+        case 'azul':
+            mensaje2 = 'Saliste desde el bloque azul'
+            break
+        default:
+            mensaje2 = 'Saliste desde otro lugar'
+    }
+
+    alert(mensaje2)
 }
