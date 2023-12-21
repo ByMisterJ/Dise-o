88130e1944378adb074ffbf2b48832aca73d3267
daw2 <josearanda9000@gmail.com>
2023-12-21 12:49:22 +0100

    commit deeventos sencillos, sin finalizar

---
+++ JavaScript/02/eventos-sencillos.js
@@ -0,0 +1,18 @@
+
+
+function primerDiv(){
+    alert("Pulsaste sobre el div")
+}
+
+function primerButton(){
+    alert("Pulsaste sobre el botón")
+}
+
+function primerAlert(){
+    if(document.getElementById("div2")){
+        alert("Pulsaste sobre el segundo div")
+    }else if(document.getElementById("boton2")){
+        alert("Pulsaste sobre el segundo botón")
+    }
+    
+}
